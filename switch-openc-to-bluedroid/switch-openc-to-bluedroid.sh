3befe2f2399e1df8331af42c1ab7d8f8a2c6684f
micgeri <micgeri@users.noreply.github.com>
2016-01-14 18:49:38 -0400

    switch-openc-to-bluedroid/switch-openc-to-bluedroid.sh : Mise entre guillemet des variables.

--- switch-openc-to-bluedroid/switch-openc-to-bluedroid.sh
+++ switch-openc-to-bluedroid/switch-openc-to-bluedroid.sh
@@ -20,11 +20,11 @@
 
 
 ### Variables ###
-BASE_DIR=`dirname $(readlink -f $0)`
-OPENC_KK_DIR=$BASE_DIR/P821E10_KITKAT
-IN_DIR=$BASE_DIR/put-files-here
-TMP_DIR=$BASE_DIR/tmp
-TESTW_FILE=$BASE_DIR/testwrite
+BASE_DIR=`dirname "$(readlink -f $0)"`
+OPENC_KK_DIR="$BASE_DIR/P821E10_KITKAT"
+IN_DIR="$BASE_DIR/put-files-here"
+TMP_DIR="$BASE_DIR/tmp"
+TESTW_FILE="$BASE_DIR/testwrite"
 
 if [[ $1 == "-h" ||  $1 == "--help" ]]; then
         echo "Ce script permet d'envoyer les fichiers nécessaires à la prise en charge de bluedroid sur l'Open C" &&
@@ -37,10 +37,10 @@ fi
 
 (
 	# Vérification des droits en écriture sur le dossier courant
-	(touch $TESTW_FILE 2>/dev/null && rm $TESTW_FILE 2>/dev/null) || (echo "$0 : Merci de donner les droits en écriture au dossier de ce script" && exit 1)
+	(touch "$TESTW_FILE" 2>/dev/null && rm "$TESTW_FILE" 2>/dev/null) || (echo "$0 : Merci de donner les droits en écriture au dossier de ce script" && exit 1)
 
 	# Vérification de l'existence du fichier boot.img
-	if [[ ! -f $IN_DIR/boot.img ]]; then
+	if [[ ! -f "$IN_DIR/boot.img" ]]; then
 		echo "$0 : Merci de copier le fichier boot.img modifié dans le dossier $IN_DIR" && exit 1
 	fi
 
@@ -51,30 +51,32 @@ fi
 	fi
 
 	# Re-création de la structure
-	rm -rf $TMP_DIR && mkdir $TMP_DIR
+	echo "$0 : Confirmer l'exécution de la commande -> rm -rvI \"$TMP_DIR\"" &&
+	rm -rvI "$TMP_DIR" && mkdir "$TMP_DIR"
 
 	# Extraction du zip contenant les fichiers
 	echo "$0 : Extraction de l'archive contenant les premiers fichiers pour bluedroid..." &&
-	unzip $BASE_DIR/openc-system-files.zip -d $TMP_DIR >/dev/null &&
+	unzip "$BASE_DIR/openc-system-files.zip" -d "$TMP_DIR" >/dev/null &&
 
 	# Téléchargement et extraction de l'archive contenant les fichiers
-	echo "$0 : Téléchargement et extraction si nécessaire de l'archive Kitkat pour Open C..."
-	if [[ ! -d $OPENC_KK_DIR && ! -f $IN_DIR/kk.zip ]]; then
+	if [[ ! -d "$OPENC_KK_DIR" && ! -f "$IN_DIR/kk.zip" ]]; then
 		(
-			wget -nv -O $IN_DIR/kk.zip http://download.ztedevice.com/UpLoadFiles/product/643/4880/soft/2014101309394339.zip &&
-			unzip $IN_DIR/kk.zip -d $TMP_DIR >/dev/null &&
-			find $TMP_DIR/ -name update.zip -exec unzip {} -d $OPENC_KK_DIR \;
+			echo "$0 : Téléchargement et extraction si nécessaire de l'archive Kitkat pour Open C..." &&
+			wget -nv -O "$IN_DIR/kk.zip" http://download.ztedevice.com/UpLoadFiles/product/643/4880/soft/2014101309394339.zip &&
+			unzip "$IN_DIR/kk.zip" -d "$TMP_DIR" >/dev/null &&
+			find "$TMP_DIR/" -name update.zip -exec unzip {} -d "$OPENC_KK_DIR" \;
 		) || (echo "$0 : Le téléchargement a échoué, merci de réessayer" && exit 1)
-	elif [[ ! -d $OPENC_KK_DIR ]]; then
+	elif [[ ! -d "$OPENC_KK_DIR" ]]; then
 		(
-			unzip $IN_DIR/kk.zip -d $TMP_DIR >/dev/null &&
-			find $TMP_DIR/ -name update.zip -exec unzip {} -d $OPENC_KK_DIR \;
+			echo "$0 : Extraction si nécessaire de l'archive Kitkat pour Open C..." &&
+			unzip "$IN_DIR/kk.zip" -d "$TMP_DIR" >/dev/null &&
+			find "$TMP_DIR/" -name update.zip -exec unzip {} -d "$OPENC_KK_DIR" \;
 		) || (echo "$0 : Echec de l'extraction du fichier" && exit 1)
 	fi
 
 	echo "$0 : Récupération des blobs..." &&
-	mkdir -p $TMP_DIR/system/vendor/lib &&
-	cp -p $OPENC_KK_DIR/system/vendor/lib/libbt* $TMP_DIR/system/vendor/lib/ &&
+	mkdir -p "$TMP_DIR/system/vendor/lib" &&
+	cp -p "$OPENC_KK_DIR"/system/vendor/lib/libbt* "$TMP_DIR/system/vendor/lib/" &&
 	
 	# Arrêt de Firefox OS et remontage de la partition system
 	adb shell stop b2g &&
@@ -82,12 +84,12 @@ fi
 
 	# Envoi de l'image boot sur la mémoire interne du téléphone et application de celle-ci
 	echo "$0 : Envoi des fichiers sur le téléphone..." &&
-	adb push $IN_DIR/boot.img /storage/sdcard/boot_bth.img >/dev/null &&
+	adb push "$IN_DIR/boot.img" /storage/sdcard/boot_bth.img >/dev/null &&
 	adb shell dd if=/storage/sdcard/boot_bth.img of=/dev/block/mmcblk0p7 >/dev/null &&
 	adb shell rm /storage/sdcard/boot_bth.img
 
 	# Envoi des fichiers sur le téléphone
-	adb push $TMP_DIR/system /system >/dev/null 2>&1 &&
+	adb push "$TMP_DIR/system" /system >/dev/null 2>&1 &&
 
 	# Attributions des utilisateurs/droits sur les fichiers
 	echo "$0 : Attribution des permissions sur les fichiers..." &&
